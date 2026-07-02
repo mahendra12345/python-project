22182ce2a49ac804bc15a01e3a2ff892cfe60c1c
mahendra <mahendra.dahat09@gmail.com>
2026-07-02 17:43:29 +0530

    added the new urls in url.py

--- masteraicloud/urls.py
+++ masteraicloud/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('master/', include('master.urls')),
+    path('/', include('master.urls')),
 ]
